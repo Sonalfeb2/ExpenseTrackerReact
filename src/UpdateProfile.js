fb54479ef63577993f6a69b26a86c7efb263617d
Sonal Agrawal <sonalfeb7@gmail.com>
2023-12-12 20:09:43 +0530

    Created Complete Profile Form

--- src/UpdateProfile.js
+++ src/UpdateProfile.js
@@ -1,8 +1,24 @@
-const UpdateProfile  = () =>{
-    return(
-        <div>
-          <h1>  Update Info</h1>
-        </div>
-    )
-}
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const UpdateProfile = () => {
+  
+  return (
+    <Container>
+      <h2>Contact Details</h2>
+      <Row className="justify-content-md-center">
+        <Col>
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control size="sm" type="text" />
+
+          <Form.Label>Photo Url</Form.Label>
+          <Form.Control size="sm" type="text" />
+        </Col>
+      </Row>
+
+      <Button variant="danger" className="mt-3" type="submit">
+        Submit
+      </Button>
+    </Container>
+  );
+};
 export default UpdateProfile;
