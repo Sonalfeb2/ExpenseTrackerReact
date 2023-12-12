5bcd3097f893445eafa348f8827ad5a07d506fe2
Sonal Agrawal <sonalfeb7@gmail.com>
2023-12-12 17:35:46 +0530

    Created Login Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Signup from "./Auth/Signup";
+import {useState} from 'react';
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const userLoginHandler = () =>{
+    setUserLoggedIn(true)
+  }
   return (
     <div className="App">
-      <Signup/>
+      {!userLoggedIn && <Signup userLogin= {userLoginHandler} />}
+      {userLoggedIn && <h1>Welcome to Expense Tracker !!</h1>}
     </div>
   );
 }
--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,59 +1,94 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-import { Alert, Toast , ToastContainer } from "react-bootstrap";
+import { Alert, Toast, ToastContainer } from "react-bootstrap";
 import { useRef, useState } from "react";
-function Signup() {
+function Signup(props) {
   const emailInputRef = useRef();
   const passInputRef = useRef();
   const confirmInputRef = useRef();
   const [showError, setShowError] = useState({ active: false, message: "" });
-  const SubmitHandler = async(e) => {
+  const [showloginpage, setShowloginpage] = useState(false);
+  const SubmitHandler = async e => {
     e.preventDefault();
     let pass = passInputRef.current.value;
-    let cpass = confirmInputRef.current.value;
-    if (pass === cpass) {
-        const response = await fetch(
-            `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCYNOpI0OEGmCAOJsAmLIIFICI97RaoUK8
-            `,
-            {
-              method: "POST",
-              headers: {
-                "content-type": "application/json"
-              },
-              body: JSON.stringify({
-                email: emailInputRef.current.value,
-                password: passInputRef.current.value,
-                returnSecureToken: true
-              })
-            }
-          );
-          const data = await response.json();
-          if (data.error) {
-            setShowError({ active: true, message: data.error.message });
-            setTimeout(() => setShowError({ active: false, message: "" }), 3000);
-            emailInputRef.current.value = "";
-            passInputRef.current.value = "";
-          }
-          else{
-            setShowError({ active: "true", message: "Login SuccessFully" });
-            setTimeout(() => setShowError({ active: false, message: "" }), 3000);
-            emailInputRef.current.value = "";
-            passInputRef.current.value = "";
-          }
+    let email = emailInputRef.current.value;
+    if (showloginpage) {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCYNOpI0OEGmCAOJsAmLIIFICI97RaoUK8",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({
+            email: email,
+            password: pass,
+            returnSecureToken: true
+          })
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data.error) {
+        setShowError({ active: true, message: data.error.message });
+        setTimeout(() => setShowError({ active: false, message: "" }), 3000);
+        emailInputRef.current.value = "";
+        passInputRef.current.value = "";
+      } else {
+        setShowError({ active: "true", message: "login SuccessFully" });
+        setTimeout(() => setShowError({ active: false, message: "" }), 3000);
+        props.userLogin();
+        emailInputRef.current.value = "";
+        passInputRef.current.value = "";
+      }
     } else {
-      setShowError({
-        active: true,
-        message: "Password doesnot match with confirm"
-      });
-      setTimeout(() => setShowError({ active: false, message: "" }), 3000);
-      passInputRef.current.value= '';
-      confirmInputRef.current.value=''
+      let cpass = confirmInputRef.current.value;
+      if (pass === cpass) {
+        const response = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCYNOpI0OEGmCAOJsAmLIIFICI97RaoUK8`,
+          {
+            method: "POST",
+            headers: {
+              "content-type": "application/json"
+            },
+            body: JSON.stringify({
+              email: email,
+              password: pass,
+              returnSecureToken: true
+            })
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        if (data.error) {
+          setShowError({ active: true, message: data.error.message });
+          setTimeout(() => setShowError({ active: false, message: "" }), 3000);
+          emailInputRef.current.value = "";
+          passInputRef.current.value = "";
+        } else {
+          setShowError({ active: "true", message: "Signup SuccessFully" });
+          setTimeout(() => setShowError({ active: false, message: "" }), 3000);
+          emailInputRef.current.value = "";
+          passInputRef.current.value = "";
+          confirmInputRef.current.value = "";
+        }
+      } else {
+        setShowError({
+          active: true,
+          message: "Password doesnot match with confirm"
+        });
+        setTimeout(() => setShowError({ active: false, message: "" }), 3000);
+        passInputRef.current.value = "";
+        confirmInputRef.current.value = "";
+      }
     }
   };
+  console.log(showloginpage);
   return (
-    <>
-    {showError.active&& <ToastContainer
+    <div>
+      {showError.active &&
+        <ToastContainer
           className="p-3"
           position="top-center"
           style={{ zIndex: 1 }}
@@ -66,54 +101,65 @@ function Signup() {
               {showError.message}
             </Toast.Body>
           </Toast>
-        </ToastContainer>
-}
-    <Card
-      style={{ width: "20rem", marginTop: "10%" }}
-      className=" mx-auto"
-      onSubmit={SubmitHandler}
-    >
-      <Form className="mx-auto">
-        <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            required
-            ref={emailInputRef}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+        </ToastContainer>}
+      <Card
+        style={{ width: "20rem", marginTop: "10%" }}
+        className=" mx-auto"
+        onSubmit={SubmitHandler}
+      >
+        {showloginpage
+          ? <p className="text-center w-100" variant="info">
+              Login
+            </p>
+          : <p className="text-center w-100">Signup</p>}
+        <Form className="mx-auto">
+          <Form.Group className="mb-3 mt-2" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              required
+              ref={emailInputRef}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            required
-            ref={passInputRef}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            ref={confirmInputRef}
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" className="mb-3" type="submit">
-          Submit
-        </Button>
-      </Form>
-      <Alert variant="info" sm="2">
-        Have an Account ? Login
-      </Alert>
-    </Card>
-    </>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              required
+              ref={passInputRef}
+            />
+          </Form.Group>
+          {!showloginpage &&
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                ref={confirmInputRef}
+                required
+              />
+            </Form.Group>}
+          <Button variant="primary" className="mb-3" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Alert
+          variant="info"
+          sm="2"
+          onClick={() => setShowloginpage(!showloginpage)}
+        >
+          {showloginpage
+            ? <p>Create an account ? Signup</p>
+            : <p>Have an Account ? Login</p>}
+        </Alert>
+      </Card>
+    </div>
   );
 }
 
